51f9da2481ab6901414fa813f7a789b5103d3af7
ShakaPursuit <shakalivngstone@pursuit.org>
2023-11-16 21:49:51 -0500

    added header component, continuing to build out frontend

---
+++ src/componenets/LandingPage.jsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom"
+
+const Header=()=>{
+    return (
+
+        <header>
+            <strong>Music Connection</strong>
+
+
+        </header>
+    )
+
+
+
+
+
+}
+
+export default Header
